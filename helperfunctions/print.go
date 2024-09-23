e275d68b5b5eafaed18026a46a0f987bb898d95e
yosaidi <younes.saidi.23@ump.ac.ma>
2024-09-23 12:06:59 +0100

    ascii-art

---
+++ helperfunctions/print.go
@@ -0,0 +1,39 @@
+package ascii
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Print(input *string) {
+	str := *input
+	lines := strings.Split(str, "\n")
+	myString := ReadFile("standard")
+
+	for i, line := range lines {
+
+		if line == "" {
+			if i == len(lines)-1 {
+				continue
+			}
+			fmt.Println()
+			continue
+		}
+		newInput := SpaceManager(line)
+
+		for row := 1; row < 9; row++ {
+			for _, word := range newInput {
+				wordRunes := []rune(word)
+				for j := 0; j < len(wordRunes); j++ {
+					char := wordRunes[j]
+					starts := (int(char) - 32) * 9
+					fmt.Print(myString[starts+row])
+				}
+
+			}
+			fmt.Println()
+		}
+
+	}
+	*input= str
+}
--- main.go
+++ main.go
@@ -21,36 +21,7 @@ func main() {
 		fmt.Println("invalid character in your input  ")
 		return
 	}
-	myString := ascii.ReadFile("standard")
-	if len(os.Args) == 3 {
-		myString = ascii.ReadFile(os.Args[2])
-	}
-	lines := strings.Split(input, "\n")
-
-	for i, line := range lines {
-
-		if line == "" {
-			if i == len(lines)-1 {
-				continue
-			}
-			fmt.Println()
-			continue
-		}
-		newInput := ascii.SpaceManager(line)
-
-		for row := 1; row < 9; row++ {
-			for _, word := range newInput {
-				wordRunes := []rune(word)
-				for j := 0; j < len(wordRunes); j++ {
-					char := wordRunes[j]
-					starts := (int(char) - 32) * 9
-					fmt.Print(myString[starts+row])
-				}
-
-			}
-			fmt.Println()
-		}
-
-	}
+	
+	ascii.Print(&input)
 
 }
