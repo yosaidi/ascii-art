dd2b73aa6c887e8285514b02613ef4710e8a0bbf
Younes Saidi <saidiyounes150@gmail.com>
2024-11-09 11:36:11 +0100

    Update print.go

--- helperfunctions/print.go
+++ helperfunctions/print.go
@@ -7,27 +7,18 @@ import (
 	"strings"
 )
 
-func Print(input *string, filename,font string) {
+func Print(input *string, font string) {
 	str := *input
 	lines := strings.Split(str, "\n")
 	myString := ReadFile(font)
 
-	// Create or open the file for writing
-	file, err := os.Create(filename)
-	if err != nil {
-		fmt.Println("Error creating file:", err)
-		return
-	}
-	defer file.Close()
-
-	writer := bufio.NewWriter(file)
 
 	for i, line := range lines {
 		if line == "" {
 			if i == len(lines)-1 {
 				continue
 			}
-			writer.WriteString("\n")
+			fmt.Println()
 			continue
 		}
 		newInput := SpaceManager(line)
@@ -38,14 +29,12 @@ func Print(input *string, filename,font string) {
 				for j := 0; j < len(wordRunes); j++ {
 					char := wordRunes[j]
 					starts := (int(char) - 32) * 9
-					writer.WriteString(myString[starts+row])
+					fmt.Print(myString[starts+row])
 				}
 			}
-			writer.WriteString("\n")
+		fmt.Println()
 		}
 	}
 
-	// Flush the buffer to the file
-	writer.Flush()
 	*input = str
 }
